1ed29cf13c93ba46fae7d3422880eab0dafcd8db
Chris Desch <cdesch@gmail.com>
2017-09-09 17:31:30 -0400

    Initial Commit

---
+++ ExampleAddFilesToCPPProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ExampleAddFilesToCPPProject
+//
+//  Created by cj on 9/9/17.
+//  Copyright © 2017 KickinEspresso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
